50baaaacf745c63c7950c7c3374f1a844a09ef98
Danil Tukanov <danil.tukanov00@gmail.com>
2025-11-23 12:07:53 +0500

    added silly memes changed box position

--- thirdapp/thirdapp/app.R
+++ thirdapp/thirdapp/app.R
@@ -8,16 +8,21 @@ ui <- page_navbar(
   title = "training example",
   layout_sidebar(
     sidebar = sidebar("sidebar"),
-    card(
-      card_header("Header"),
-      card_image("https://i.pinimg.com/236x/d9/42/66/d94266793cf8cb2220810979e0ae926f.jpg"),
-      "Card content"
-    ),
     value_box(
       title = "Value box",
       value = "100% of your attention is here",
       showcase = bsicons::bs_icon("bar-chart"),
       theme = "red"
+    ),
+    card(
+      card_header("Header"),
+      card_image("https://i.pinimg.com/236x/d9/42/66/d94266793cf8cb2220810979e0ae926f.jpg"),
+      "Card content"
+    ),
+    card(
+      card_header("Another card"),
+      card_image("https://pbs.twimg.com/media/Edi5vQHXoAArFjf.png"),
+      "Hello"
     ),
     "Main contents"
   )
